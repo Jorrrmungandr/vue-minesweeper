86c0666320064421d6125d7d049e2c82333e5f3c
wangziming <wzm2053031@gmail.com>
2024-09-15 00:06:26 +0800

    feat: implement mine grid

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,37 +3,142 @@ defineOptions({
   name: 'IndexPage',
 })
 
-function onClick(x: number, y: number) {
-  // console.log(`clicked at (${x}, ${y})`)
+interface BlockState {
+  x: number,
+  y: number
+  opened: boolean
+  flagged: boolean
+  isMine: boolean
+  adjacentMines: number
 }
 
 const WIDTH = 10
 const HEIGHT = 10
+const DIRECTIONS = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1], [0, 1],
+  [1, -1], [1, 0], [1, 1],
+]
+const COLORS = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-yellow-500',
+  'text-pink-500',
+  'text-indigo-500',
+  'text-cyan-500',
+]
+const DEV = false
+let firstClick = true
 
 const state = reactive(Array.from(
-  Array.from({ length: HEIGHT }),
-  (_, row) =>
-    Array.from(Array.from({ length: WIDTH }), (_, col) => row * 10 + col + 1),
-),
+  new Array(HEIGHT), (_, x) =>
+    Array.from(new Array(WIDTH), (_, y) => <BlockState> { // 这里是ts的尖括号assertion
+      x, y,
+      opened: false,
+      flagged: false,
+      isMine: false,
+      adjacentMines: 0,
+      }
+    ),
+  ),
 )
+
+function getBlockClass(block: BlockState) {
+  if (!block.opened) {
+    return 'bg-gray-600/10'
+  }
+  return block.isMine
+    ? 'bg-red/50'
+    : COLORS[block.adjacentMines]
+}
+
+function onClick(block: BlockState) {
+  if (firstClick) {
+    generateMines(block)
+    firstClick = false
+  }
+  block.opened = true
+  if (block.isMine) {
+    // alert('BOOOOOM!')
+  }
+}
+
+function updateMineCounts() {
+  state.forEach((row, x) => {
+    row.forEach((item, y) => {
+      if (item.isMine) {
+        return
+      }
+      let count = 0
+      DIRECTIONS.forEach(([dx, dy]) => {
+        const x2 = x + dx // 不能让x自增，因为x，y后续还要使用
+        const y2 = y + dy
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT) {
+          return
+        }
+        if (state[x2][y2].isMine) {
+          count++
+        }
+      })
+      item.adjacentMines = count
+    })
+  })
+}
+
+function generateMines(firstBlock: BlockState) {
+  for (const row of state) {
+    for (const block of row) {
+      // 以30%的概率生成地雷
+      if (Math.abs(firstBlock.x - block.x) <= 1) {
+        if (Math.abs(firstBlock.y - block.y) <= 1) {
+          continue
+        }
+      }
+      block.isMine = Math.random() < 0.3
+    }
+  }
+  updateMineCounts()
+}
+
+
 </script>
 
 <template>
   <div>
+    <div mb-5>
     Minesweeper
+    </div>
 
     <div
-      v-for="row in HEIGHT"
-      :key="row"
+      v-for="(row, x) in state"
+      :key="x"
+      flex="~"
+      items-start justify-center
     >
       <button
-        v-for="col in WIDTH"
-        :key="col"
-        h-10 w-10 border hover:bg-gray
-        @click="onClick(row, col)"
+        v-for="(item, y) in row"
+        :key="y"
+        h-10 w-10 m-0.5 hover="bg-gray/60"
+        border="~ white/10"
+        flex
+        items-center justify-center
+        :class="getBlockClass(item)"
+        @click="onClick(item)"
       >
-        {{ state[row][col] }}
+        <template v-if="item.opened || DEV">
+          <div v-if="item.isMine" i-mdi-mine />
+          <div v-else>
+            {{ item.adjacentMines }}
+          </div>
+        </template>
+        <template v-else>
+
+        </template>
       </button>
     </div>
   </div>
+
 </template>
